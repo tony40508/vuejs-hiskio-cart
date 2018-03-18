eb53dd00c8f65811f1eef261f143bbc9eedfac34
takeu <tony40508@gmail.com>
2018-03-19 03:39:56 +0800

    :heavy_plus_sign: add Header.vue

---
+++ src/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header">
+    <span class="cart">
+      <span> 購物車內： </span>
+      <span class="lessons"> {{ lessons }} </span>
+      <span> 套課程，共 </span>
+      <span class="price"> {{ price }} </span>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    lessons () {
+      return 0;
+    },
+    price () {
+      return 0;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  position: fixed;
+  width: 100%;
+  padding: 6px 8px;
+  box-sizing: border-box;
+  background-color: #41b883;
+  font-size: .9em;
+  text-align: center;
+  color: white;
+  z-index: 3;
+}
+
+.lessons, .price {
+  color: #ffc
+}
+</style>
