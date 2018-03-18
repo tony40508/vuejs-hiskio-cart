9c475c3cb90b885c626388c3276e1759a9306d7b
takeu <tony40508@gmail.com>
2018-03-19 00:45:45 +0800

    :heavy_plus_sign: add Lessonlist.vue

---
+++ src/LessonList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div
+      v-for="lesson in lessons"
+      :key="lesson.id"
+    >
+      <span>{{ lesson.title }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      lessons () {
+        return this.$store.state.lessons;
+      }
+    },
+    mounted () {
+      this.$store.dispatch('fetchLessons');
+    }
+  }
+</script>
